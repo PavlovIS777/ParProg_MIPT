d80b6f6228b949b339bda66b1923cbea4e8e2318
Pavlov, Ivan <pavlov.is@stratasolutions.ru>
2025-03-28 01:38:50 +0300

    lel

---
+++ task1.cpp
@@ -0,0 +1,10 @@
+#include <mpi.h>
+#include <stdio.h>
+int main(int argc, char *argv[]){
+    int rank, count;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &count);
+    printf("Hello World, rank: %d, count: %d", rank, count);
+    MPI_Finalize();
+}
